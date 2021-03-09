38b936c8ac58586f558021944dd753a046915098
XaviP <pezbaloo@riseup.net>
2021-03-09 12:49:39 +0100

    translate resp to obj properties

--- utils/account.py
+++ utils/account.py
@@ -1,35 +1,24 @@
-from datetime import datetime
-
 from utils.api_services import get_user_accounts, get_account_balance
-from utils.api_services import get_account_transfers, get_unknown_accounts
+from utils.api_services import get_account_transfers  # , get_unknown_accounts
 from utils.transfer import Transfer
 
 
 def get_accounts(access):
-    resp = get_user_accounts(access)
-    user_id = resp["data"]["id"]
+    data = get_user_accounts(access)
     accounts = []
-    for member in resp["data"]["relationships"]["members"]["data"]:
-        accounts.append(Account(user_id, member["id"]))
-    for incl in resp["included"]:
-        if incl["type"] == "members":
-            for a in accounts:
-                if a.member_id == incl["id"]:
-                    a.member_name = incl["attributes"]["name"]
-                    a.member_image = incl["attributes"]["image"]
-                    a.acc_id = incl["relationships"]["account"]["data"]["id"]
-                    a.acc_code = incl["attributes"]["code"]
-                    a.acc_link = \
-                        incl["relationships"]["account"]["links"]["related"]
-                    a.group_id = incl["relationships"]["group"]["data"]["id"]
-                    a.group_code = a.acc_link.split("/")[-3]
+    for account in data["accounts"]:
+        account_obj = Account(data["user_id"])
+        for key, value in account.items():
+            setattr(account_obj, key, value)
+        accounts.append(account_obj)
+
     return accounts
 
 
 class Account:
-    def __init__(self, user_id, member_id):
+    def __init__(self, user_id):
         self.user_id = user_id
-        self.member_id = member_id
+        self.member_id = ""
         self.member_name = ""
         self.member_image = ""
         self.acc_id = ""
@@ -45,44 +34,38 @@ class Account:
         self.currency_decimals = 0
 
     def get_balance(self, access):
-        resp = get_account_balance(access, self.acc_link)
-        self.balance = resp["data"]["attributes"]["balance"]
-        self.currency_id = resp["included"][0]["id"]
-        self.currency_name = resp["included"][0]["attributes"]["name"]
-        self.currency_plural = resp["included"][0]["attributes"]["namePlural"]
-        self.currency_symbol = resp["included"][0]["attributes"]["symbol"]
-        self.currency_decimals = resp["included"][0]["attributes"]["decimals"]
+        data = get_account_balance(access, self.acc_link)
+        for key, value in data.items():
+            setattr(self, key, value)
 
     def get_transfers(self, access):
-        resp = get_account_transfers(access, self.group_code, self.acc_id)
+        data = get_account_transfers(access, self.group_code, self.acc_id)
         transfers = []
         unknown_accounts = []
-        for trans in resp["data"]:
-            if trans["type"] == "transfers":
-                t = Transfer(trans["id"])
-                t.amount = trans["attributes"]["amount"]
-                t.meta = trans["attributes"]["meta"]
-                t.state = trans["attributes"]["state"]
-                t.created = datetime.fromisoformat(
-                    trans["attributes"]["created"])
-                t.updated = datetime.fromisoformat(
-                    trans["attributes"]["updated"])
-                t.payer_acc_id = trans["relationships"]["payer"]["data"]["id"]
-                if t.payer_acc_id == self.acc_id:
-                    t.payer_acc_code = self.acc_code
-                else:
-                    unknown_accounts.append(t.payer_acc_id)
-                t.payee_acc_id = trans["relationships"]["payee"]["data"]["id"]
-                if t.payee_acc_id == self.acc_id:
-                    t.payee_acc_code = self.acc_code
-                else:
-                    unknown_accounts.append(t.payee_acc_id)
-                t.currency_id = self.currency_id
+        for trans in data:
+            t = Transfer(trans["transfer_id"])
+            for key, value in trans.items():
+                setattr(t, key, value)
+            if t.payer_acc_id == self.acc_id:
+                t.payer_acc_code = self.acc_code
+            else:
+                unknown_accounts.append(t.payer_acc_id)
+            if t.payee_acc_id == self.acc_id:
+                t.payee_acc_code = self.acc_code
+            else:
+                unknown_accounts.append(t.payee_acc_id)
+            if t.currency_id == self.currency_id:
                 t.currency_name = self.currency_name
                 t.currency_plural = self.currency_plural
                 t.currency_symbol = self.currency_symbol
                 t.currency_decimals = self.currency_decimals
-                transfers.append(t)
+            else:
+                # transfer currency is not the same as account.
+                # could this happen ??
+                pass
+
+            transfers.append(t)
+
         # TODO: get unknown accounts
         # resp2 = get_unknown_accounts(access, self.group_code,
         #                              unknown_accounts)
--- utils/api_services.py
+++ utils/api_services.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from .oauth2 import KomunitinNetError
 
 
@@ -7,7 +8,32 @@ def get_user_accounts(access):
               "/social/users/me?include=members")
     resp = requests.get(me_url, headers=access.headers)
     if resp.status_code == 200:
-        return resp.json()
+        r = resp.json()
+        data = {
+            "user_id": r["data"]["id"],
+            "accounts": []
+        }
+        for member in r["data"]["relationships"]["members"]["data"]:
+            data["accounts"].append({
+                "user_id": data["user_id"],
+                "member_id": member["id"]
+            })
+        for i in r["included"]:
+            if i["type"] == "members":
+                for a in data["accounts"]:
+                    if a["member_id"] == i["id"]:
+                        a["member_name"] = i["attributes"]["name"]
+                        a["member_image"] = i["attributes"]["image"]
+                        a["acc_id"] = \
+                            i["relationships"]["account"]["data"]["id"]
+                        a["acc_code"] = i["attributes"]["code"]
+                        a["acc_link"] = (i["relationships"]["account"]
+                                         ["links"]["related"])
+                        a["group_id"] = \
+                            i["relationships"]["group"]["data"]["id"]
+                        a["group_code"] = a["acc_link"].split("/")[-3]
+        return data
+
     else:
         print("Error %s: %s" % (resp.status_code, resp.text))
         raise KomunitinNetError(resp.text, resp.status_code)
@@ -17,7 +43,16 @@ def get_account_balance(access, acc_link):
     acc_url = "{}?{}".format(acc_link, "include=currency")
     resp = requests.get(acc_url, headers=access.headers)
     if resp.status_code == 200:
-        return resp.json()
+        r = resp.json()
+        data = {
+            "balance": r["data"]["attributes"]["balance"],
+            "currency_id": r["included"][0]["id"],
+            "currency_name": r["included"][0]["attributes"]["name"],
+            "currency_plural": r["included"][0]["attributes"]["namePlural"],
+            "currency_symbol": r["included"][0]["attributes"]["symbol"],
+            "currency_decimals": r["included"][0]["attributes"]["decimals"],
+        }
+        return data
     else:
         print("Error %s: %s" % (resp.status_code, resp.text))
         raise KomunitinNetError(resp.text, resp.status_code)
@@ -29,7 +64,25 @@ def get_account_transfers(access, group_code, account_id):
     resp = requests.get(trans_url.format(group_code, account_id),
                         headers=access.headers)
     if resp.status_code == 200:
-        return resp.json()
+        r = resp.json()
+        data = []
+        for t in r["data"]:
+            if t["type"] == "transfers":
+                trans = {
+                    "transfer_id": t["id"],
+                    "amount": t["attributes"]["amount"],
+                    "meta": t["attributes"]["meta"],
+                    "state": t["attributes"]["state"],
+                    "created": datetime.fromisoformat(
+                        t["attributes"]["created"]),
+                    "updated": datetime.fromisoformat(
+                        t["attributes"]["updated"]),
+                    "payer_acc_id": t["relationships"]["payer"]["data"]["id"],
+                    "payee_acc_id": t["relationships"]["payee"]["data"]["id"],
+                    "currency_id": t["relationships"]["currency"]["data"]["id"]
+                }
+                data.append(trans)
+        return data
     else:
         print("Error %s: %s" % (resp.status_code, resp.text))
         raise KomunitinNetError(resp.text, resp.status_code)

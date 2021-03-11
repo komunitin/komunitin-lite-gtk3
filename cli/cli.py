4afdd903b7b99f7e307182ab0583ccf1dd8729fe
XaviP <pezbaloo@riseup.net>
2021-03-11 08:47:19 +0100

    cli as an object

--- cli/cli.py
+++ cli/cli.py
@@ -1,64 +1,74 @@
 import sys
 import getpass
-import datetime
 
 from utils.oauth2 import ApiAccess
-from utils.api_services import get_user_accounts, get_account_balance
-from utils.api_services import get_account_statement
+from utils.account import get_user_accounts
 
 
-def command_line_interface(config):
-    print("Connecting...", end='\r', flush=True)
-    access = ApiAccess(config)
-    if not access.has_access:
-        _authenticate(access)
-    try:
-        members, accounts, groups = get_user_accounts(access)
-    except Exception as e:
-        print(str(e))
-        sys.exit()
-    print(_("Account") + ": {}".format(members[0]["code"]) + " " * 10)
-    print(_("Getting account info") + "...", end='\r', flush=True)
-    try:
-        balance, currency = get_account_balance(
-            access, groups[0]["code"], members[0]["code"])
-    except Exception as e:
-        print(str(e))
-        sys.exit()
-    print(_("Balance") + ": {} {}".format(
-        balance, currency["symbol"]) + " " * 10)
-    print(_("Getting last transactions") + "...", end='\r', flush=True)
-    try:
-        transfers = get_account_statement(
-            access, groups[0]["code"], accounts[0]["id"])
-    except Exception as e:
-        print(str(e))
-        sys.exit()
-    for trans in transfers:
-        created = datetime.datetime.fromisoformat(
-            trans["attributes"]["created"])
-        amount = str(trans["attributes"]["amount"]) + " " + currency["symbol"]
-        print("{}| {}| {}".format(
-            created.strftime("%d/%m/%Y").ljust(10),
-            trans["attributes"]["meta"].ljust(40),
-            amount.ljust(10)
-        ))
+class CommandLineInterface:
+    def __init__(self, config):
+        self.config = config
+        self.access = None
+        self.accounts = []
+        self.account = None
+        self.transfers = []
 
+    def run(self):
+        print("Connecting...", end='\r', flush=True)
+        self.access = ApiAccess(self.config)
+        if not self.access.has_access:
+            self._authenticate()
+        self._get_accounts()
+        self._get_transfers()
 
-def _authenticate(access):
-    ok = False
-    while not ok:
-        message = _("Email")
-        message += " (" + access.user + "): " if access.user else ": "
-        user_input = input(message)
-        password = getpass.getpass(_("Password") + ": ")
-        if user_input:
-            user = user_input
-        print(_("Authenticating") + "...", end='\r', flush=True)
-        ok, error = access.new_access(user, password)
-        if not ok:
-            if error[0:17] == "Wrong credentials":
-                print(_("Wrong credentials"))
-            if error[0:7] == "Network":
-                print(_("Network error"))
-                sys.exit()
+    def _authenticate(self):
+        ok = False
+        while not ok:
+            message = _("Email")
+            if self.access.user:
+                message += " ({})".format(self.access.user)
+            user_input = input("{}: ".format(message))
+            password = getpass.getpass(_("Password") + ": ")
+            user = user_input if user_input else self.access.user
+            print(_("Authenticating") + "...", end='\r', flush=True)
+            ok, error = self.access.new_access(user, password)
+            if not ok:
+                if error[0:17] == "Wrong credentials":
+                    print(_("Wrong credentials"))
+                if error[0:7] == "Network":
+                    print(_("Network error"))
+                    sys.exit()
+
+    def _get_accounts(self):
+        try:
+            self.accounts = get_user_accounts(self.access)
+        except Exception as e:
+            print(str(e))
+            sys.exit()
+        self.account = self.accounts[0]
+        print(_("Account") + ": {}".format(
+              self.account.account["code"]) + " " * 10)
+        print(_("Getting account info") + "...", end='\r', flush=True)
+        try:
+            self.account.get_balance(self.access)
+        except Exception as e:
+            print(str(e))
+            sys.exit()
+        print(_("Balance") + ": {} {}".format(
+              self.account.balance,
+              self.account.currency["symbol"]) + " " * 10)
+
+    def _get_transfers(self):
+        print(_("Getting last transactions") + "...", end='\r', flush=True)
+        try:
+            self.transfers = self.account.get_transfers(self.access)
+        except Exception as e:
+            print(str(e))
+            sys.exit()
+        for trans in self.transfers:
+            print("{}| {}| {}".format(
+                trans.created.strftime("%d/%m/%Y").ljust(10),
+                trans.meta.ljust(40),
+                "{} {}".format(trans.amount,
+                               trans.currency["symbol"]).ljust(10)
+            ))
